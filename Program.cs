f51c88fd7fe10ca332083af29202f907a77d1c9e
Kodis0 <ekimta110@gmail.com>
2023-12-14 17:59:12 +0300

    Добавление нового класса для хранения книг, также был добавлен метод возвращения в консоль всех книг

--- Program.cs
+++ Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿using System;
+using StoreLib;
+
+namespace Program
+{
+
+    public class Book
+    {
+        public string Title { get; }
+
+        public int Id { get; }
+
+        public string Author { get; }
+
+        public int Year { get; }
+
+        public string Publish { get; }
+
+        public string Describe { get; }
+
+        public int Price { get; }
+
+        public Book(string title, int id, string author, int year, string publish, string describe, int price)
+        {
+            Title = title;
+            Id = id;
+            Author = author;
+            Year = year;
+            Publish = publish;
+            Describe = describe;
+            Price = price;
+        }
+        static void Main(string[] args)
+        {
+            // Создаем экземпляр BookRepository
+            BookRepository bookRepository = new BookRepository();
+
+            // Получаем список всех книг
+            List<Book> allBooks = bookRepository.GetAllBooks();
+            Console.WriteLine("Список всех книг:");
+            foreach (var book in allBooks)
+            {
+                Console.WriteLine($"Название: {book.Title}\n\n Автор: {book.Author}\n Год издания: {book.Year}\n Издательство: {book.Publish}\n Стоимость: {book.Price}\n\n Описание: {book.Describe}\n\n");
+            }
+        }
+    }
+
+
+}
